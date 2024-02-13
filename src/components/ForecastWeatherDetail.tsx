b0219df55cad51cae3b462509ef198f23e9905a1
ArturT <mr.arturio@gmail.com>
2024-02-13 13:38:29 -0500

    fix spelling

--- src/components/ForecastWeatherDetail.tsx
+++ src/components/ForecastWeatherDetail.tsx
@@ -5,7 +5,7 @@ import WeatherDetails, { WeatherDetailProps } from "./WeatherDetails";
 import { KelvinToCelsius } from "@/utils/KelvinToCelsius";
 
 export interface ForecastWeatherDetailProps extends WeatherDetailProps {
-  weatehrIcon: string;
+  weatherIcon: string;
   date: string;
   day: string;
   temp: number;
@@ -19,7 +19,7 @@ export default function ForecastWeatherDetail(
   props: ForecastWeatherDetailProps
 ) {
   const {
-    weatehrIcon = "02d",
+    weatherIcon = "02d",
     date = "19.09",
     day = "Tuesday",
     temp,
@@ -33,7 +33,7 @@ export default function ForecastWeatherDetail(
       {/* left */}
       <section className=" flex gap-4 items-center px-4  ">
         <div className=" flex flex-col gap-1 items-center">
-          <WeatherIcon iconName={weatehrIcon} />
+          <WeatherIcon iconName={weatherIcon} />
           <p>{date}</p>
           <p className="text-sm">{day} </p>
         </div>
