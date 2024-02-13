300200abf71f9255af2f11b552ab04e9913973c1
ArturT <mr.arturio@gmail.com>
2024-02-13 12:01:33 -0500

    color update

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -7,7 +7,7 @@ type Props = {};
 
 export default function Navbar({}: Props) {
   return (
-    <nav className="shadow-sm sticky top-0 left-0 z-50 bg-white">
+    <nav className="shadow-sm sticky top-0 left-0 z-50 bg-blue-200/80">
       <div className="h-[80px] w-full flex justify-between items-center max-w-screen-2xl px-3 mx-auto">
         <p className="flex items-center justify-center gap-2">
           <h2 className="text-gray-500 text-3xl font-bold">Weather App</h2>
